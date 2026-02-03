51232e5339b9de2c11a3eb320156a623d97bdda1
WhereAmI-toaa <zqbhtgvzxf@privaterelay.appleid.com>
2026-02-02 21:05:11 -0500

    Update static export

---
+++ _next/static/chunks/pages/_app-d3bba541588a274d.js
@@ -0,0 +1,9 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[636],{50:()=>{},92:(e,n,a)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return a(8761)}])},564:()=>{},1678:()=>{},7328:(e,n,a)=>{e.exports=a(8022)},8761:(e,n,a)=>{"use strict";a.r(n),a.d(n,{default:()=>s});var t=a(7876),r=a(7328),l=a.n(r);function s({Component:e,pageProps:n}){let a="andreabellipanni".replace(/^\/+/,"").replace(/\/+$/,""),r=a?`/${a}`:"",s=`${r}/fonts/HandmadeAlphabet.otf`;return(0,t.jsxs)(t.Fragment,{children:[(0,t.jsx)(l(),{children:(0,t.jsx)("style",{children:`
+          @font-face {
+            font-family: 'HandmadeAlphabet';
+            src: url('${s}') format('opentype');
+            font-weight: 400;
+            font-style: normal;
+            font-display: swap;
+          }
+        `})}),(0,t.jsx)(e,{...n})]})}a(1678),a(50),a(564),a(9384)},9384:()=>{}},e=>{var n=n=>e(e.s=n);e.O(0,[593,792],()=>(n(92),n(9603))),_N_E=e.O()}]);
